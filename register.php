beada0d169abb81100e2df583ad4980d3d50e7fa
SugarRu <andreyritter@rambler.ru>
2020-02-22 14:56:45 +1100

    Registration validation

--- register.php
+++ register.php
@@ -20,7 +20,7 @@
     $_SESSION['password'] = $_POST['password'];
     $_SESSION['password_confirmation'] = $_POST['password_confirmation'];
 
-    if ($_SESSION['password'] == $_SESSION['password_confirmation']) {
+    if ($_SESSION['password'] == $_SESSION['password_confirmation'] && $_SESSION['password'] != 0) {
 
         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
         $pdo = new PDO('mysql:host=localhost;dbname=marlin_php', 'root', '', $options);
@@ -28,10 +28,16 @@
         $sql = "INSERT INTO registration (name, email, password) VALUES (:name, :email, :password)";
         
         $STH = $pdo->prepare($sql);
+
+        $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+
+
         $STH->execute(array (
             'name' => $_POST['name'],
-            'email' => $_POST['email'],
-            'password' => $_POST['password']
+            'email' => $email,
+            'password' => $hash
         ));  
 
         $_SESSION['success'] = 'Успешная регистрация';
@@ -40,7 +46,7 @@
 
         $_SESSION['error_pass'] = 'Пароли не совпадают';
         $_SESSION['name'] = $_POST['name'];
-        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['email'] =  $_POST['email'];
         
     }    
     
@@ -52,7 +58,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand" href="index.php">
                     Project
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -72,7 +78,7 @@
                                 <a class="nav-link" href="login.html">Login</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="register.html">Register</a>
+                                <a class="nav-link" href="register.php">Register</a>
                             </li>
                     </ul>
                 </div>
@@ -86,6 +92,21 @@
                         <div class="card">
                             <div class="card-header">Register</div>
 
+                            <?php
+                            
+                            if ($_SESSION['success']) {
+                                echo '<span class="alert-success text-md-center" role="alert"><strong> '
+                                . $_SESSION['success'] . '</strong></span>';
+                                unset($_SESSION['name']);
+                                unset($_SESSION['success']);
+                                unset($_SESSION['email']);
+                                unset($_SESSION['password']);
+                                unset($_SESSION['password_confirmation']);
+                                
+                            }                                          
+
+                            ?>
+
                             <div class="card-body">
                                 <form method="POST" action="">
 
@@ -93,29 +114,12 @@
                                         <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
                                         <div class="col-md-6">
-                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" autofocus 
-                                            <?php 
-                                             if ($_SESSION['success']) {
-                                                unset($_SESSION['name']);
-                                             }
-                                            ?> 
-                                            value="<?php echo $_SESSION['name'] ?>">
+                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" autofocus value="<?php echo $_SESSION['name']?>">
 
                                             <?php 
-
-                                                if ($_SESSION['error_pass']) {
-                                                    echo '<span class="invalid-feedback" role="alert"><strong>' . $_SESSION['error_pass'] . '</strong></span>';
-                                                    unset($_SESSION['error_pass']);
-                                                }
-
-                                                if ($_SESSION['success']) {
-                                                    echo '<span class=" alert-success"" role="alert"><strong> '
-                                                    . $_SESSION['success'] . '</strong></span>';
-                                                    unset($_SESSION['success']);
-                                                    unset($_SESSION['email']);
-                                                    unset($_SESSION['password']);
-                                                }                                          
-
+                                                if (empty($_POST['name'])) {
+                                                    echo '<span class="invalid-feedback" role="alert"><strong>' . "Введите имя" . '</strong></span>';
+                                                }                                                
                                             ?>
 
                                         </div>
@@ -126,6 +130,12 @@
 
                                         <div class="col-md-6">
                                             <input id="email" type="email" class="form-control" name="email" value="<?php echo $_SESSION['email'] ?>">
+
+                                            <?php 
+                                                if (empty($_POST['email'])) {
+                                                    echo '<span class="invalid-feedback" role="alert"><strong>' . "Введите e-mail" . '</strong></span>';
+                                                }
+                                            ?>
                                         </div>
                                     </div>
 
@@ -134,6 +144,12 @@
 
                                         <div class="col-md-6">
                                             <input id="password" type="password" class="form-control " name="password" value="<?php echo $_SESSION['password'] ?>" autocomplete="new-password">
+
+                                            <?php 
+                                                if (empty($_POST['password'])) {
+                                                    echo '<span class="invalid-feedback" role="alert"><strong>' . "Введите пароль" . '</strong></span>';
+                                                }
+                                            ?>
                                         </div>
                                     </div>
 
@@ -142,12 +158,20 @@
 
                                         <div class="col-md-6">
                                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
+
+                                            <?php
+                                            if ($_SESSION['error_pass'] && $_SESSION['password'] != 0) {
+                                                echo '<span class="invalid-feedback" role="alert"><strong>' . $_SESSION['error_pass'] . '</strong></span>';
+                                                unset($_SESSION['error_pass']);
+                                                unset($_SESSION['password_confirmation']);
+                                            }
+                                            ?>
                                         </div>
                                     </div>
 
                                     <div class="form-group row mb-0">
                                         <div class="col-md-6 offset-md-4">
-                                            <button type="submit" class="btn btn-primary">
+                                            <button type="submit" class="btn btn-primary" name="submit">
                                                 Register
                                             </button>
                                         </div>
@@ -162,3 +186,12 @@
     </div>
 </body>
 </html>
+
+<!--  <?php 
+
+if ($_SESSION['error_pass'] && $_SESSION['password'] != 0) {
+    echo '<span class="invalid-feedback" role="alert"><strong>' . $_SESSION['error_pass'] . '</strong></span>';
+    unset($_SESSION['error_pass']);
+    unset($_SESSION['password_confirmation']);
+}
+?>  -->
