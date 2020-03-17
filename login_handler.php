615829a8152dabacc175989139ee49a6c44e6f30
SugarRu <andreyritter@rambler.ru>
2020-03-18 07:41:00 +1100

    Session & Cookie setting via checkbox

--- login_handler.php
+++ login_handler.php
@@ -1,11 +1,10 @@
 <?php
-/* session_start(); */
 require 'db.php';
 
 /* Подготовка переменных */
-$email = $_SESSION['email'] = $_POST['email'];
+$email = $_SESSION['email'] = trim($_POST['email']);
 $pswrd = trim($_POST['password']);
-
+$checkbox = $_POST['remember'];
 
 /* Подготовка запроса для сверки данных */
 $sql = 'SELECT email, password FROM registration WHERE email = :email';
@@ -15,51 +14,77 @@ $STH->execute($params);
 $login_data = $STH->fetch(PDO::FETCH_ASSOC);
 
 
-/* Валидация формы */
-  if (!$email) {
-    $_SESSION['message_email'] = 'Введите почту';
-    header('Location: login.php');
 
-  } elseif ( !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
-    $_SESSION['message_email'] = 'Неверный формат';
-    header('Location: login.php');  
+# Проверка наличия сессии
+if ($_SESSION['user']) {  
+  header('Location: index.php');   
+  
+} else {# Проверка наличия куки
+  if ($_COOKIE['pswrd'] && $_COOKIE['email']) {
 
-  } elseif (!$pswrd) {
-    $_SESSION['message_pswrd'] = 'Введите пароль';
-    header('Location: login.php');  
-    
+    # Поиск пользователя в БД по куки и начало новой сессии
+
+      /* Подготовка запроса для сверки данных */
+        $sql = 'SELECT email, password FROM registration WHERE email = :email';
+        $params = [':email' => $_COOKIE['email']];
+        $STH = $pdo->prepare($sql);
+        $STH->execute($params);
+        $login_data = $STH->fetch(PDO::FETCH_ASSOC);
+      /*  */
+        
+      if ( $_COOKIE['pswrd'] == $login_data['password'] )   { 
+        $_SESSION['user'] = $login_data['email'];
+        header('Location: index.php');
+      } 
+    #   
+  } else {#авторизация
+
+    /* Валидация формы */
+      if (!$email) {
+        $_SESSION['message_email'] = 'Введите почту';
+        header('Location: login.php');
+
+      } elseif ( !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+        $_SESSION['message_email'] = 'Неверный формат';
+        header('Location: login.php');  
+
+      } elseif (!$pswrd) {
+        $_SESSION['message_pswrd'] = 'Введите пароль';
+        header('Location: login.php');     
    
-  }  else  {
+      }  else  {
 
-    /* Свверка данных */
-    if (!$login_data) {
-      $_SESSION['message_email'] = 'Данная почта не зарегестрированна';
-      header('Location: login.php');
+        /* Свверка данных */
+          if (!$login_data) {
+            $_SESSION['message_email'] = 'Данная почта не зарегестрированна';
+            header('Location: login.php');
 
-    } elseif (!password_verify($pswrd, $login_data['password'])) { // сверка хеша пароля {
-      $_SESSION['message_pswrd'] = 'Неверный пароль';
-      header('Location: login.php');
+          } elseif (!password_verify($pswrd, $login_data['password'])) { // Cверка хеша пароля {
+            $_SESSION['message_pswrd'] = 'Неверный пароль';
+            header('Location: login.php');
 
-    } else {
-      $_SESSION['user'] = $login_data['email'];
+          } else {
+            $_SESSION['user'] = $login_data['email'];
 
-      unset($_SESSION['message_email'],
-       $_SESSION['email'],
-       $_SESSION['message_pswrd']
-      );      
+            unset($_SESSION['message_email'],
+            $_SESSION['email'],
+            $_SESSION['message_pswrd']
+            );
       
-      header('Location: index.php');   
-    }   
-  }
-  /* Конец сверки данных */ 
-/*  */
+            #Установка куки по чекбоксу
+              if ($checkbox == 1) {
+                setcookie('email', $email, time() + (3600 * 24 * 30));                
+                setcookie('pswrd', $login_data['password'], time() + (3600 * 24 * 30));
 
-
-
-  
-  
-
-
- 
-
- 
+              } else {
+                setcookie('email', '', time()-3600);
+                setcookie('pswrd', '', time()-3600);
+              }
+            #
+            header('Location: index.php');   
+          }  
+        /* Конец сверки данных */   
+      }         
+    /* Конец валидации формы */
+  } 
+}
